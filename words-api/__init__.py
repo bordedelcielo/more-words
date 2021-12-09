bfdc82916e9674d49c0f0fb0e4a883b0ae33ad9a
bordedelcielo <christopher.thrutchley@gmail.com>
2021-12-09 16:12:56 -0600

    stable

--- words-api/__init__.py
+++ words-api/__init__.py
@@ -11,17 +11,8 @@ app = Flask (__name__)
 
 app.register_blueprint(site)
 
-# Insert blueprints here.
-
 app.config.from_object(Config)
 
-# root_db.init_app(app)
-
 migrate = Migrate(app, root_db)
-
-# login_manager.init_app(app)
-# login_manager.login_view = 'auth.signin'
-
-# ma.init_app(app)
 
 app.json_encoder = JSONEncoder

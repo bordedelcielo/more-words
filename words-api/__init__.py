0823c5e892ed9703c7fcb07edc5994c9a845aff1
bordedelcielo <christopher.thrutchley@gmail.com>
2021-12-09 16:47:22 -0600

    2nd commit, working auth.signup

--- words-api/__init__.py
+++ words-api/__init__.py
@@ -6,10 +6,12 @@ from config import Config
 from .models import db as root_db
 from .helpers import JSONEncoder
 from .site.routes import site
+from .authentication.routes import auth
 
 app = Flask (__name__)
 
 app.register_blueprint(site)
+app.register_blueprint(auth)
 
 app.config.from_object(Config)
 

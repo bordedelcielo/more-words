47d674e204bd275bcc7031a5f3f65af67bde8301
bordedelcielo <christopher.thrutchley@gmail.com>
2021-12-12 20:59:13 -0600

    able to query db for users

--- words_api/my_words/routes.py
+++ words_api/my_words/routes.py
@@ -9,6 +9,7 @@ prod = Blueprint('prod', __name__, template_folder = 'my_words_templates')
 @prod.route('/mywords')
 @login_required
 def mywords():
-    cursor.execute("select * from public.user")
+    cursor.execute("select username from public.user")
+    # cursor.execute("select username from public.user")
     result = cursor.fetchall()
     return render_template('mywords.html', data=result)

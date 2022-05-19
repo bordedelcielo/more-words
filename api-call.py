9cf062d13935dd490b11488d04c3f7da7906a6dd
bordedelcielo <christopher.thrutchley@gmail.com>
2022-05-18 23:46:27 -0500

    free dictionary api working again, added sample query

--- api-call.py
+++ api-call.py
@@ -1,12 +1,13 @@
-# import requests
-# import json
+import requests
+import json
 
-# word = "panettone"
-# response = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+# free dictionary api
+# https://dictionaryapi.dev/
+# https://github.com/meetDeveloper/freeDictionaryAPI
 
-# # print(response.text)
-# # print(response.json()[0])
-# # print(response.json()[0]["word"])
-# # print(response.json()[0]["origin"])
-# # print(response.json()[0])
-# print(response.json()[0]["meanings"][0]["definitions"][0]["definition"])
+word = "panettone"
+response = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+
+print(response.json()[0]["meanings"][0]["definitions"][0]["definition"])
+
+# Lingua Robot api

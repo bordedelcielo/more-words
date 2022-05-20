b7c8c6389e2b584e1357977a093b17aed40d943b
bordedelcielo <christopher.thrutchley@gmail.com>
2022-05-20 15:16:39 -0500

    updated port name

--- words_api/cursor.py
+++ words_api/cursor.py
@@ -5,6 +5,6 @@ database = os.environ.get('DATABASE')
 user = os.environ.get('USER')
 password = os.environ.get('PASSWORD')
 host = os.environ.get('HOST')
-port = os.environ.get('PORT')
+port = os.environ.get('DB_PORT')
 
 con = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)

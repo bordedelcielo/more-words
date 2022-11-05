cf82bea734686323d69c0a4879a5771e97dd2372
Gerard Keiser <gerardkeiser@gmail.com>
2022-11-05 17:12:55 -0500

    Add install instructions to readme

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 A Flask website for managing word definitions.
 
 Hosted on Glitch here: https://cream-dust-beanie.glitch.me/
+
+## Local Install
+
+1. Use `env` for the name of your venv folder.
+2. Register for the [Lingua Robot](https://rapidapi.com/rokish/api/lingua-robot/) API and set the values for `X-RapidAPI-Key` and `X-RapidAPI-Host` for environment variables `API_KEY` and `API_HOST`, respectively.
+3. Set the environment variable `FLASK_APP=words_api`.
+4. By default it will use an SQLite database at `app.db`. If you want to use postgres, set the environment variable `SQLALCHEMY_DATABSE_URI=postgresql://your_db`.
+5. `flask db init`
+6. `flask db migrate -m "Initial migration"`
+7. `flask db upgrade`
+8. `flask run`
--- words_api/my_words/routes.py
+++ words_api/my_words/routes.py
@@ -5,23 +5,26 @@ from words_api.forms import AddWordForm
 from words_api.headers import headers
 from words_api.models import Word, db
 
-prod = Blueprint('prod', __name__, template_folder = 'my_words_templates')
+prod = Blueprint("prod", __name__, template_folder="my_words_templates")
 
-@prod.route('/mywords', methods = ['GET', 'POST'])
+
+@prod.route("/mywords", methods=["GET", "POST"])
 @login_required
 def mywords():
     username = session["username"]
     form = AddWordForm()
 
     try:
-        if request.method == 'POST':
+        if request.method == "POST":
             word = form.word.data
             url = f"https://lingua-robot.p.rapidapi.com/language/v1/entries/en/{word}"
             response = requests.request("GET", url, headers=headers)
 
             if response.status_code == 200:
 
-                definition = response.json()["entries"][0]["lexemes"][0]["senses"][0]["definition"]
+                definition = response.json()["entries"][0]["lexemes"][0]["senses"][0][
+                    "definition"
+                ]
                 added_by_user = username
 
                 entry = Word(word, definition, added_by_user)
@@ -29,12 +32,19 @@ def mywords():
                 db.session.add(entry)
                 db.session.commit()
 
-                flash(f"{word.title()} added to {added_by_user}'s word list. {word.title()}: {definition}", 'user-created')
+                flash(
+                    f"{word.title()} added to {added_by_user}'s word list. {word.title()}: {definition}",
+                    "user-created",
+                )
             else:
-                flash(f'The word \'{word.title()}\' did not appear in our search. Please check the spelling of the word you entered or try another word.')
+                flash(
+                    f"The word '{word.title()}' did not appear in our search. Please check the spelling of the word you entered or try another word."
+                )
 
     except:
         # raise Exception('Invalid Form Data: Please check your form')
-        flash(f'The word \'{word}\' did not appear in our search. Please check the spelling of the word you entered or try another word.')
+        flash(
+            f"The word '{word}' did not appear in our search. Please check the spelling of the word you entered or try another word."
+        )
 
-    return render_template('mywords.html', form = form)
+    return render_template("mywords.html", form=form)
